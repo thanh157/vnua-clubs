3afdacbbb117205c679168140f0357bbd6679d30
thanh157 <hd.thanh157@gmail.com>
2025-01-10 02:03:41 +0700

    refactor: ui/thanhhtd/fixs

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,20 +15,20 @@ class DatabaseSeeder extends Seeder
         // Tạo tài khoản admin
         $this->call(AdminSeeder::class);
 
-        // Tạo 100 user ngẫu nhiên
-        $this->call(UserSeeder::class);
+        // // Tạo 100 user ngẫu nhiên
+        // $this->call(UserSeeder::class);
 
-        // Tạo 10 câu lạc bộ
-        $this->call(ClubSeeder::class);
+        // // Tạo 10 câu lạc bộ
+        // $this->call(ClubSeeder::class);
         
-        // Tạo 50 hoạt động ngẫu nhiên cho các câu lạc bộ
-        $this->call(ActivitySeeder::class);
+        // // Tạo 50 hoạt động ngẫu nhiên cho các câu lạc bộ
+        // $this->call(ActivitySeeder::class);
 
-        // Tạo 200 bài viết ngẫu nhiên
-        $this->call(PostSeeder::class);
+        // // Tạo 200 bài viết ngẫu nhiên
+        // $this->call(PostSeeder::class);
 
-        // Tạo 500 thành viên ngẫu nhiên cho các câu lạc bộ
-        $this->call(MemberSeeder::class);
+        // // Tạo 500 thành viên ngẫu nhiên cho các câu lạc bộ
+        // $this->call(MemberSeeder::class);
 
     }
 }
